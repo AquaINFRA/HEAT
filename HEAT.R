92d07eb3533cd35999b9039f82d8725e1f69be0b
Hjalte Parner <hjalte@ices.dk>
2020-08-24 13:38:06 +0200

    Making the assessment dynamic

--- HEAT.R
+++ HEAT.R
@@ -7,86 +7,21 @@ ipak <- function(pkg){
     install.packages(new.pkg, dependencies = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
-packages <- c("sf", "data.table", "ggplot2", "tidyverse")
+packages <- c("sf", "data.table", "ggplot2", "tidyverse", "mapview")
 ipak(packages)
 
-# ES = Eutrophication Status
-# SD = Standard Deviation
-# N = Number of Observations
-# N_Min = Minimum Number of Observations any given year 
-# SE = Standards Error
-# CI = Confidense Interval 95 %
-# ER = Eutrophication Ratio
-# BEST
-# EQR = Ecological Quality Ratio
-# EQR_HG
-# EQR_GM
-# EQR_MP
-# EQR_PB
-# EQRS = Ecological Quality Ratio Scaled
-# TCG
-# TCS
-# TC
-# SCG
-# SCS
-# SC
-# IW
+# Util functions ---------------------------------------------------------------
+download.file.unzip.maybe <- function(url, refetch = FALSE, path = ".") {
+  dest <- file.path(path, sub("\\?.+", "", basename(url)))
+  if (refetch || !file.exists(dest)) {
+    download.file(url, dest, mode = "wb")
+    if (tools::file_ext(dest) == "zip") {
+      unzip(dest, exdir = path)
+    }
+  }
+}
 
-# Links to Unit, Data and Config files ----------------------------------------- 
-#unitsFile <- "https://www.dropbox.com/s/tpzirhsr11yuwb4/AssessmentUnit_20112016.zip"
-#stationSamplesFile <- "https://www.dropbox.com/s/ehbb50myea9ebx8/StationSamplesICE.zip"
-#indicatorFile <- "https://www.dropbox.com/s/3vw8uhxseyk24yo/Indicator.txt"
-#indicatorUnitFile <- "https://www.dropbox.com/s/iiqfjsg6fvyx5ux/IndicatorUnit.txt"
-
-unitsFile <- "assessment/20112016/AssessmentUnit_20112016.zip"
-stationSamplesFile <- "assessment/20112016/StationSamplesICE.zip"
-indicatorFile <- "assessment/20112016/Indicator.txt"
-indicatorUnitFile <- "assessment/20112016/IndicatorUnit.txt"
-
-indicator <- fread(input = indicatorFile, sep = "\t")
-indicatorUnit <- fread(input = indicatorUnitFile, sep = "\t")
-
-# Assessment Units -------------------------------------------------------------
-
-# Read assessment unit from shapefile
-units <- st_read(unitsFile)
-
-# Filter for open sea assessment units
-units <- units[units$Code %like% 'SEA',]
-
-# Include stations from position 55.86667+-0.01667 12.75+-0.01667 which will include the Danish station KBH/DMU 431 and the Swedish station Wlandskrona into assessment unit 3/SEA-003
-units[3,] <- st_union(units[3,],st_as_sfc("POLYGON((12.73333 55.85,12.73333 55.88334,12.76667 55.88334,12.76667 55.85,12.73333 55.85))", crs = 4326))
-#units[units$Code == 'SEA-003',] <- st_union(units[units$Code == 'SEA-003',],st_as_sfc("POLYGON((12.73333 55.85,12.73333 55.88334,12.76667 55.88334,12.76667 55.85,12.73333 55.85))", crs = 4326))
-
-# Convert to data.table
-#units <- as.data.table(units)
-
-# Assign IDs
-units$UnitID = 1:nrow(units)
-
-# Identify invalid geometries
-st_is_valid(units)
-
-# Transform projection into ETRS_1989_LAEA
-units <- st_transform(units, crs = 3035)
-
-# Calculate area
-units$UnitArea <- st_area(units)
-
-#setDT(units)
-#setkey(units, UnitID)
-
-# Identify invalid geometries
-st_is_valid(units)
-
-# Make geometries valid by doing the buffer of nothing trick
-#units <- sf::st_buffer(units, 0.0)
-
-# Identify overlapping assessment units
-#st_overlaps(units)
-
-# Make grid units
-make_gridunits <- function(units, gridSize)
+make.gridunits <- function(units, gridSize)
 {
   units <- st_transform(units, crs = 3035)
   
@@ -109,26 +44,78 @@ make_gridunits <- function(units, gridSize)
   
   gridunits$Area <- st_area(gridunits)
   
-  #setDT(gridunits)
-  
-  #gridunits <- gridunits[order(GridID, UnitID),.(GridID, UnitID, Area, geometry)]
-
   return(gridunits)
 }
 
-gridunits10 <- make_gridunits(units, 10000)
+# Download and unpack files needed for the assessment --------------------------
+urls <- c("https://www.dropbox.com/s/s79psj46ua61dao/AssessmentUnits_20112016.zip?dl=1",
+           "https://www.dropbox.com/s/7wahao1dwcvese9/Indicators.txt?dl=1",
+           "https://www.dropbox.com/s/5lcv5j5a4wabn7p/IndicatorUnits.txt?dl=1",
+           "https://www.dropbox.com/s/ehbb50myea9ebx8/StationSamplesICE.zip?dl=1",
+           "https://www.dropbox.com/s/qjgx0b5plkwz6iv/StationSamplesCTD.zip?dl=1")
 
-gridunits30 <- make_gridunits(units, 30000)
+assessment <- "20112016"
+path <- paste0("Assessment/", assessment)
+dir.create(path, showWarnings = FALSE, recursive = TRUE)
+files <- sapply(urls, download.file.unzip.maybe, path = path)
+
+assessmentUnitsFile <- file.path(path, paste0("AssessmentUnits_", assessment, ".shp"))
+indicatorsFile <- file.path(path, "Indicators.txt")
+indicatorUnitsFile <- file.path(path, "IndicatorUnits.txt")
+stationSamplesICEFile <- file.path(path, "StationSamplesICE.txt")
+
+# Assessment Units + Grid Units-------------------------------------------------
+
+# Read assessment unit from shapefile
+units <- st_read(assessmentUnitsFile)
+
+# Filter for open sea assessment units
+units <- units[units$Code %like% 'SEA',]
+
+# Include stations from position 55.86667+-0.01667 12.75+-0.01667 which will include the Danish station KBH/DMU 431 and the Swedish station Wlandskrona into assessment unit 3/SEA-003
+units[3,] <- st_union(units[3,],st_as_sfc("POLYGON((12.73333 55.85,12.73333 55.88334,12.76667 55.88334,12.76667 55.85,12.73333 55.85))", crs = 4326))
+#units[units$Code == 'SEA-003',] <- st_union(units[units$Code == 'SEA-003',],st_as_sfc("POLYGON((12.73333 55.85,12.73333 55.88334,12.76667 55.88334,12.76667 55.85,12.73333 55.85))", crs = 4326))
+
+# Convert to data.table
+#units <- as.data.table(units) %>% st_sf()
+
+# Assign IDs
+units$UnitID = 1:nrow(units)
+
+# Identify invalid geometries
+st_is_valid(units)
+
+# Transform projection into ETRS_1989_LAEA
+units <- st_transform(units, crs = 3035)
+
+# Calculate area
+units$UnitArea <- st_area(units)
+
+# Identify invalid geometries
+st_is_valid(units)
+
+# Make geometries valid by doing the buffer of nothing trick
+#units <- sf::st_buffer(units, 0.0)
+
+# Identify overlapping assessment units
+#st_overlaps(units)
+
+# Make grid units
+gridunits10 <- make.gridunits(units, 10000)
+
+gridunits30 <- make.gridunits(units, 30000)
 
 # Plot
-ggplot() + geom_sf(data = units) + coord_sf()
-ggplot() + geom_sf(data = gridunits10) + coord_sf()
-ggplot() + geom_sf(data = gridunits30) + coord_sf()
+#ggplot() + geom_sf(data = units) + coord_sf()
+#ggplot() + geom_sf(data = gridunits10) + coord_sf()
+#ggplot() + geom_sf(data = gridunits30) + coord_sf()
 
 # Read stationSamples ----------------------------------------------------------
-stationSamples <- fread(input = stationSamplesFile, sep = "\t", na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE, check.names = TRUE)
+stationSamples <- fread(input = stationSamplesICEFile, sep = "\t", na.strings = "NULL", stringsAsFactors = FALSE, header = TRUE, check.names = TRUE)
 
 # !!! We could separate stations from samples here, do the spatial and then rejoin samples again ... migth be more rubust
+#stationSamples[, StationID := .GRP, by = .(Cruise, StationNumber, Year, Month, Day, Hour, Minute, Latitude..degrees_north., Longitude..degrees_east.)]
+#stationSamples[, .N, .(ID, Cruise, StationNumber, Year, Month, Day, Hour, Minute, Latitude..degrees_north., Longitude..degrees_east.)]
 
 # Make stations spatial keeping original latitude/longitude
 stationSamples <- st_as_sf(stationSamples, coords = c("Longitude..degrees_east.", "Latitude..degrees_north."), remove = FALSE, crs = 4326)
@@ -140,7 +127,6 @@ stationSamples <- st_transform(stationSamples, crs = 3035)
 stationSamples$UnitID <- st_intersects(stationSamples, units) %>% as.numeric()
 
 # Classify stations into 10k gridunits
-#stations <- st_intersection(stations, gridunits10 %>% select(GridID.10k = GridID, Area.10k = Area))
 stationSamples <- st_join(stationSamples, gridunits10 %>% select(GridID.10k = GridID, Area.10k = Area), join = st_intersects)
 
 # Classify stations into 30k gridunits
@@ -149,9 +135,74 @@ stationSamples <- st_join(stationSamples, gridunits30 %>% select(GridID.30k = Gr
 # Remove spatial column
 stationSamples <- st_set_geometry(stationSamples, NULL)
 
-# Read indicator configs
-indicator <- fread(input = indicatorFile, sep = "\t") %>% setnames("ID", "IndicatorID") %>% setkey(IndicatorID) 
-indicatorUnit <- fread(input = indicatorUnitFile, sep = "\t") %>% setkey(IndicatorID, UnitID)
+# Read indicator configs -------------------------------------------------------
+indicators <- fread(input = indicatorsFile, sep = "\t") %>% setkey(IndicatorID) 
+indicatorUnits <- fread(input = indicatorUnitsFile, sep = "\t") %>% setkey(IndicatorID, UnitID)
+
+# Loop indicator units ---------------------------------------------------------
+for (i in 1:nrow(indicatorUnits)) {
+  indicatorID <- indicatorUnits[i, IndicatorID]
+  
+  
+  
+}
+
+# Loop indicators --------------------------------------------------------------
+for(i in 1:nrow(indicators)){
+  indicatorID <- indicators[i, IndicatorID]
+  criteriaID <- indicators[i, CriteriaID]
+  categoryID <- 0
+  year.min <- indicators[i, YearMin]
+  year.max <- indicators[i, YearMax]
+  month.min <- indicators[i, MonthMin]
+  month.max <- indicators[i, MonthMax]
+  depth.min <- indicators[i, DepthMin]
+  depth.max <- indicators[i, DepthMax]
+
+  # Copy data
+  wk <- as.data.table(stationSamples)
+
+  # Create Period
+  wk[, Period := ifelse(month.min > month.max & Month >= month.min, Year + 1, Year)]
+
+  # Create Indicator
+  if (indicatorID == 1) { # Dissolved Inorganic Nitrogen
+    wk$ES <- apply(wk[, list(Nitrate..umol.l., Nitrite..umol.l., Ammonium..umol.l.)], 1, function(x){
+      if (all(is.na(x)) | is.na(x[1])) {
+        NA
+      }
+      else {
+        sum(x, na.rm = TRUE)
+      }
+    })
+  }
+  else if (indicatorID == 2) { # Dissolved Inorganic Phosphorus
+    wk[,ES := Phosphate..umol.l.]
+  }
+  else if (indicatorID == 3) { # Cholorophyll a
+    wk[, ES := Chlorophyll.a..ug.l.]
+  }
+
+  # Filter stations rows and columns --> UnitID, GridID, GridArea, Period, Month, StationID, Depth, ES
+  if (month.min > month.max) {
+    wk0 <- wk[
+        (Period >= year.min & Period <= year.max) &
+        (Month >= month.min | Month <= month.max) &
+        (Depth..m.db..PRIMARYVAR.DOUBLE >= depth.min & Depth..m.db..PRIMARYVAR.DOUBLE <= depth.max) &
+        !is.na(ES) & 
+        !is.na(UnitID),
+        .(IndicatorID = indicatorID, UnitID, GridID = ifelse(UnitID == 3, GridID.10k, GridID.30k), GridArea = ifelse(UnitID == 3, Area.10k, Area.30k), Period, Month, StationID, Depth = Depth..m.db..PRIMARYVAR.DOUBLE, ES)]
+  } else {
+    wk0 <- wk[(Period >= year.min & Period <= year.max) &
+                (Month >= month.min & Month <= month.max) &
+                (Depth..m.db..PRIMARYVAR.DOUBLE >= depth.min & Depth..m.db..PRIMARYVAR.DOUBLE <= depth.max) &
+                !is.na(ES) & 
+                !is.na(UnitID),
+              .(IndicatorID = indicatorID, UnitID, GridID = ifelse(UnitID == 3, GridID.10k, GridID.30k), GridArea = ifelse(UnitID == 3, Area.10k, Area.30k), Period, Month, StationID, Depth = Depth..m.db..PRIMARYVAR.DOUBLE, ES)]
+  }
+
+
+}
 
 # Dissolved inorganic Nitrogen - DIN (Winter) ----------------------------------
 #   Parameters: [NO3-N] + [NO2-N] + [NH4-N]
@@ -161,9 +212,6 @@ indicatorUnit <- fread(input = indicatorUnitFile, sep = "\t") %>% setkey(Indicat
 
 # Copy data
 wk <- as.data.table(stationSamples)
-
-# Count unique stations
-wk[,.(count = uniqueN(StationID))]
 
 # Create grouping variable
 wk$Period <- with(wk, ifelse(Month == 12, Year + 1, Year))
@@ -176,13 +224,13 @@ coalesce <- function(x) {
     sum(x, na.rm = TRUE)
   } 
 }
-wk$DIN..umol.l. <- apply(wk[, list(Nitrate..umol.l., Nitrite..umol.l., Ammonium..umol.l.)], 1, coalesce)
+wk$ES <- apply(wk[, list(Nitrate..umol.l., Nitrite..umol.l., Ammonium..umol.l.)], 1, coalesce)
 
-# Filter stations rows and columns --> UnitID, GridID, GridArea, Period, Month, Depth, ES
-wk0 <- wk[Depth..m.db..PRIMARYVAR.DOUBLE <= 10 & (Month >= 12 | Month <= 2) & (Period >= 2011 & Period <= 2016) & !is.na(DIN..umol.l.), .(IndicatorID = 1, UnitID, GridID = ifelse(UnitID == 3, GridID.10k, GridID.30k), GridArea = ifelse(UnitID == 3, Area.10k, Area.30k), Period, Month, StationID, Depth = Depth..m.db..PRIMARYVAR.DOUBLE, ES = DIN..umol.l.)]
+# Filter stations rows and columns --> UnitID, GridID, GridArea, Period, Month, StationID, Depth, ES
+wk0 <- wk[(Period >= 2011 & Period <= 2016) & (Month >= 12 | Month <= 2) & Depth..m.db..PRIMARYVAR.DOUBLE <= 10 & !is.na(ES) & !is.na(UnitID), .(IndicatorID = 1, UnitID, GridID = ifelse(UnitID == 3, GridID.10k, GridID.30k), GridArea = ifelse(UnitID == 3, Area.10k, Area.30k), Period, Month, StationID, Depth = Depth..m.db..PRIMARYVAR.DOUBLE, ES)]
 
 # Calculate station mean --> UnitID, GridID, GridArea, Period, Month, ES, SD, N
-wk1 <- wk0[, .(ES = mean(ES), SD = sd(ES), N = .N), keyby = .(IndicatorID, UnitID, GridID, GridArea,Period, Month, StationID)]
+wk1 <- wk0[, .(ES = mean(ES), SD = sd(ES), N = .N), keyby = .(IndicatorID, UnitID, GridID, GridArea, Period, Month, StationID)]
 
 # Calculate annual mean --> UnitID, Period, ES, SD, N, NM
 wk2 <- wk1[, .(ES = mean(ES), SD = sd(ES), N = .N, NM = uniqueN(Month)), keyby = .(IndicatorID, UnitID, Period)]
@@ -228,7 +276,9 @@ rm(a,b,c,d)
 wk3[, TC := (TTC + SSC) / 2]
 
 # Calculate assessment ES --> UnitID, Period, ES, SD, N, N_MIN, NM, NS
-wk4 <- wk2[, .(Period = min(Period) * 10000 + max(Period), ES = mean(ES), SD = sd(ES), N = .N, N_MIN = min(N), NM = sum(NM), NS = sum(N)), .(IndicatorID, UnitID)]
+#wk4 <- wk2[, .(Period = min(Period) * 10000 + max(Period), ES = mean(ES), SD = sd(ES), N = .N, N_MIN = min(N), NM = sum(NM), NS = sum(N)), .(IndicatorID, UnitID)]
+wk4 <- wk3[, .(Period = min(Period) * 10000 + max(Period), ES = mean(ES), SD = sd(ES), N = .N, N_MIN = min(N), NM = sum(NM), NS = sum(N), GTC = mean(GTC), STC = mean(STC), SSC = mean(SSC)), .(IndicatorID, UnitID)]
+#wk4 <- wk3[, .(Period = min(Period) * 10000 + max(Period), ES = mean(ES), ES_SD = sd(ES), ES_N = .N, N_MIN = min(N), NM = sum(NM), NS = sum(N), ER = mean(ER), EQR = mean(EQR), EQRS = mean(EQRS), GTC = mean(GTC), STC = mean(STC), SSC = mean(SSC)), .(IndicatorID, UnitID)]
 
 # Combine with indicator and indicator unit configuration tables
 wk5 <- indicator[indicatorUnit[wk4]]
@@ -253,11 +303,11 @@ wk5[, EQRS := ifelse(EQR <= EQR_PB, (EQR - 0) * (0.2 - 0) / (EQR_PB - 0) + 0,
                                           (EQR - EQR_HG) * (1 - 0.8) / (1 - EQR_HG) + 0.8))))]
 
 # Calculate General temporal confidence (GTC) - minimum number of annual observations
-wk5[, GTC := ifelse(N_MIN > GTC_HM, 100, ifelse(N_MIN < GTC_ML, 0, 50))]
+#wk5[, GTC := ifelse(N_MIN > GTC_HM, 100, ifelse(N_MIN < GTC_ML, 0, 50))]
 
 # Calculate Specific temporal confidence (STC) - number of missing months in assessment period
 # !!! 18 month hardcoded for now which need to be change dynamically in config table to indicator years and months
-wk5[, STC := ifelse(18 - NM <= STC_HM, 100, ifelse(18 - NM > STC_ML, 0, 50))]
+wk5[, STCX := ifelse(18 - NM <= STC_HM, 100, ifelse(18 - NM > STC_ML, 0, 50))]
 
 # Calculate Total Temporal Confidence (TTC)
 wk5[, TTC := (GTC + STC) / 2]
@@ -265,17 +315,33 @@ wk5[, TTC := (GTC + STC) / 2]
 # Calculate General spatial confidence (GSC) - number of annual observations per grid cell ... and then what? annual average?
 
 # Calculate Specific spatial confidence (SSC) - area of sampled grid unit cells as a procentage to total unit area
-a <- wk1[, .N, keyby = .(IndicatorID, UnitID, GridID, GridArea)]
-b <- a[, .(GridArea = sum(as.numeric(GridArea))), keyby = .(IndicatorID, UnitID)]
-c <- as.data.table(units)[, .(UnitArea = as.numeric(UnitArea)), keyby = .(UnitID)]
-d <- c[b, on = .(UnitID = UnitID)]
-wk5 <- wk5[d[,.(UnitID, UnitArea, GridArea)], on = .(UnitID = UnitID)]
-wk5[, SSC := ifelse(GridArea / UnitArea * 100 > SSC_HM, 100, ifelse(GridArea / UnitArea * 100 < SSC_ML, 0, 50))]
-rm(a,b,c,d)
+#a <- wk1[, .N, keyby = .(IndicatorID, UnitID, GridID, GridArea)]
+#b <- a[, .(GridArea = sum(as.numeric(GridArea))), keyby = .(IndicatorID, UnitID)]
+#c <- as.data.table(units)[, .(UnitArea = as.numeric(UnitArea)), keyby = .(UnitID)]
+#d <- c[b, on = .(UnitID = UnitID)]
+#wk5 <- wk5[d[,.(UnitID, UnitArea, GridArea)], on = .(UnitID = UnitID)]
+#wk5[, SSC := ifelse(GridArea / UnitArea * 100 > SSC_HM, 100, ifelse(GridArea / UnitArea * 100 < SSC_ML, 0, 50))]
+#rm(a,b,c,d)
 
 # Calculate Total Confidence (TC)
 wk5[, TC := (TTC + SSC) / 2]
 
 # Write result to files
-fwrite(wk3, file = "assessment/20112016/Annual.csv")
-fwrite(wk5, file = "assessment/20112016/Assessment.csv")
+fwrite(wk3, file = "assessment/20112016/Annual_20200520.csv")
+fwrite(wk5, file = "assessment/20112016/Assessment_20200520.csv")
+
+# Dissolved inorganic Phosphorus - DIP (Winter) --------------------------------
+#   Parameters: [PO4-P]
+#   Depth: <= 10
+#   Period: December - February
+#   Aggregation Method: Arithmetric mean of mean by station per year
+
+wk <- as.data.table(stationSamples)
+
+# Create grouping variable
+wk$Period <- with(wk, ifelse(Month == 12, Year + 1, Year))
+
+# Filter stations rows and columns --> UnitID, GridID, GridArea, Period, Month, Depth, ES
+wk0 <- wk[Depth..m.db..PRIMARYVAR.DOUBLE <= 10 & (Month >= 12 | Month <= 2) & (Period >= 2011 & Period <= 2016) & !is.na(Phosphate..umol.l.), .(IndicatorID = 2, UnitID, GridID = ifelse(UnitID == 3, GridID.10k, GridID.30k), GridArea = ifelse(UnitID == 3, Area.10k, Area.30k), Period, Month, StationID, Depth = Depth..m.db..PRIMARYVAR.DOUBLE, ES = Phosphate..umol.l.)]
+
+mapview(list(st_geometry(units),st_geometry(stationSamples)))
--- ReadMe.md
+++ ReadMe.md
@@ -1,75 +1,46 @@
--   [HEAT assessment tool](#heat-assessment-tool)
-    -   [Oxygen Debt Indicator Calculation Scripts](#oxygen-debt-indicator-calculation-scripts)
+# HELCOM Eutrophication Assessment Tool (HEAT)
 
-HEAT assessment tool
-====================
+Assessment units are defined by HELCOM comtracting parties. An assessment units can have multiple indicators. Each indicator can have different temporal (months) and spatial (depths) coverage and reference values within the different assessment units. 
 
-Oxygen Debt Indicator Calculation Scripts
------------------------------------------
+### Assessment Units
 
-The following as an example of fitting the HEAT oxygen debt profile models to CTD or water bottle data. The code below assumes that the repository has been cloned and all work is done from the root directory `.../HEAT/`, and that the data has been downloaded and prepared via:
+### Station Samples
 
-``` r
-source("scripts/OxygenDebt/01_input.R")
-```
+### Indicator Definitions
 
-This will create the folder `model` and the file `model/input.csv`.
+### Common Indicators
 
-``` r
-# read in data
-oxy <- read.csv("model/input.csv")
-head(oxy)
-```
+- Dissolved Inorganic Nitrogen (DIN)
+- Dissolved Inorganic Phosphorus (DIP)
+- Chlorophyll a
+- Secchi Depth
+- Oxygen Debt
+- Total Nitrogen (TN)
+- Total Phosphorus (TP)
+- Cyanobacteria Bloom Index (CyaBI)
 
-    ##   ID Assessment_Unit Year Month Day Latitude Longitude Type Depth
-    ## 1 21         SEA-009 2010    12   4 55.79984  18.34617  CTD     0
-    ## 2 21         SEA-009 2010    12   4 55.79984  18.34617  CTD     1
-    ## 3 21         SEA-009 2010    12   4 55.79984  18.34617  CTD     2
-    ## 4 21         SEA-009 2010    12   4 55.79984  18.34617  CTD     3
-    ## 5 21         SEA-009 2010    12   4 55.79984  18.34617  CTD     4
-    ## 6 21         SEA-009 2010    12   4 55.79984  18.34617  CTD     5
-    ##   Temperature Salinity   Oxygen n_Oxygen max_depth Oxygen_ml
-    ## 1        6.23     7.09 11.23836       11    100.69      7.87
-    ## 2        6.23     7.08       NA       11    100.69        NA
-    ## 3        6.23     7.08       NA       11    100.69        NA
-    ## 4        6.23     7.08       NA       11    100.69        NA
-    ## 5        6.23     7.08       NA       11    100.69        NA
-    ## 6        6.23     7.08       NA       11    100.69        NA
-    ##   Oxygen_deficit        Basin surfacedepth1 surfacedepth2 salmod_wt_depth1
-    ## 1       1.035793 Gdansk Basin            10            20               50
-    ## 2             NA Gdansk Basin            10            20               50
-    ## 3             NA Gdansk Basin            10            20               50
-    ## 4             NA Gdansk Basin            10            20               50
-    ## 5             NA Gdansk Basin            10            20               50
-    ## 6             NA Gdansk Basin            10            20               50
-    ##   salmod_wt_depth2 salmod_wt salmod_halocline_lower salmod_max_depth    ES
-    ## 1              120         3                     30               40 10.54
-    ## 2              120         3                     30               40 10.54
-    ## 3              120         3                     30               40 10.54
-    ## 4              120         3                     30               40 10.54
-    ## 5              120         3                     30               40 10.54
-    ## 6              120         3                     30               40 10.54
-    ##     ET ET.SCORE weight inBSEP133
-    ## 1 8.66        H    100      TRUE
-    ## 2 8.66        H    100      TRUE
-    ## 3 8.66        H    100      TRUE
-    ## 4 8.66        H    100      TRUE
-    ## 5 8.66        H    100      TRUE
-    ## 6 8.66        H    100      TRUE
+### Abbreviations
 
-Now that the data are read in, the following code loads the `oxydebt` package for the `doonefit_full` and `plot_fit` functions.
-
-``` r
-# load libraries
-library(oxydebt)
-
-# inspect the results from one fit
-ID <- 8492
-data <- oxy[oxy$ID == ID,]
-fit <- doonefit_full(data, ID = ID)
-
-p <- plot_fit(fit, data)
-print(p)
-```
-
-![](ReadMe_files/figure-markdown_github/example_run-1.png)
+- ES = Eutrophication Status
+- ES_SD = Standard Deviation
+- ES_N = Number of Observations
+- ES_N_Min = Minimum Number of Observations any given year 
+- ES_SE = Standards Error
+- ES_CI = Confidense Interval
+- ET = Eutrophication Target / Threshold
+- ER = Eutrophication Ratio
+- ACDEV = Acceptable Deviation
+- BEST = ET / (1 + ACDEV / 100)
+- EQR = Ecological Quality Ratio
+- EQR_HG = Ecological Quality Ratio High/Good Boundary
+- EQR_GM = Ecological Quality Ratio Good/Moderate Boundary
+- EQR_MP = Ecological Quality Ratio Moderate/Poor Boundary
+- EQR_PB = Ecological Quality Ratio Poor/Bad Boundary
+- EQRS = Ecological Quality Ratio Scaled
+- GTC = General Temporal Confidence
+- STC = Specific Temporal Confindence
+- TTC = Total Temporal Confidence
+- GSC = General Spatial Confidence 
+- SSC = Specific Spatial Confidence
+- TSC = Total Spatial Confidence
+- TC = Total Confidence
