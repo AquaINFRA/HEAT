227cab44bdf9814ea8efb2adc5d3e4a1af925098
Merret <merret.buurman@igb-berlin.de>
2025-05-25 20:29:43 +0200

    First pygeoapi process file. Tested.

---
+++ pygeoapi_processes/docker_utils.py
@@ -0,0 +1,138 @@
+import os
+import subprocess
+import logging
+LOGGER = logging.getLogger(__name__)
+
+def run_docker_container(
+        docker_executable,
+        image_name,
+        script_name,
+        random_string,
+        download_dir,
+        inputs_read_only,
+        script_args
+    ):
+    LOGGER.debug('Prepare running docker container')
+
+    # Create container name
+    # Note: Only [a-zA-Z0-9][a-zA-Z0-9_.-] are allowed
+    #container_name = "%s_%s" % (image_name.split(':')[0], os.urandom(5).hex())
+    container_name = "%s_%s" % (image_name.split(':')[0], random_string)
+
+    # Define paths inside the container
+    container_out = '/out'
+    container_in_readonly = '/readonly'
+
+    # Define local paths
+    local_out = os.path.join(download_dir, "out")
+
+    # Ensure directories exist
+    os.makedirs(local_out, exist_ok=True)
+
+    # Replace paths in args:
+    sanitized_args = []
+    for arg in script_args:
+        newarg = arg
+        if inputs_read_only in arg:
+            newarg = arg.replace(inputs_read_only, container_in_readonly)
+            LOGGER.debug("Replaced argument %s by %s..." % (arg, newarg))
+        elif local_out in arg:
+            newarg = arg.replace(local_out, container_out)
+            LOGGER.debug("Replaced argument %s by %s..." % (arg, newarg))
+        sanitized_args.append(newarg)
+
+    # Prepare container command
+    # (mount volumes etc.)
+    docker_args = [
+        docker_executable, "run",
+        "--rm",
+        "--name", container_name,
+        "-v", f"{inputs_read_only}:{container_in_readonly}",
+        "-v", f"{local_out}:{container_out}",
+        "-e", f"SCRIPT={script_name}",
+        image_name,
+    ]
+    docker_command = docker_args + sanitized_args
+    LOGGER.debug('Docker command: %s' % docker_command)
+    
+    # Run container
+    try:
+        LOGGER.debug('Start running docker container')
+        result = subprocess.run(docker_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout = result.stdout.decode()
+        stderr = result.stderr.decode()
+        LOGGER.debug('Finished running docker container')
+        return result.returncode, stdout, stderr, "no error"
+
+    except subprocess.CalledProcessError as e:
+        returncode = e.returncode
+        stdout = e.stdout.decode()
+        stderr = e.stderr.decode()
+        LOGGER.error('Failed running docker container (exit code %s)' % returncode)
+        user_err_msg = get_error_message_from_docker_stderr(stderr)
+        return returncode, stdout, stderr, user_err_msg
+
+
+def get_error_message_from_docker_stderr(stderr, log_all_lines = True):
+    '''
+    We would like to return meaningful messages to users. For example, by
+    printing ALL stderr lines, we get the following:
+
+    ERROR - Docker stderr: Error in if (zz[which.max(zz)] < minpts) stop("All species do not have enough data after removing missing values and duplicates.") : 
+    ERROR - Docker stderr:   argument is of length zero
+    ERROR - Docker stderr: Calls: pred_extract
+    ERROR - Docker stderr: Execution halted
+
+    ERROR - Docker stderr: Error in pred_extract(data = speciesfiltered, raster = worldclim, lat = in_colname_lat,  : 
+    ERROR - Docker stderr:   All species do not have enough data after removing missing values and duplicates.
+    ERROR - Docker stderr: Execution halted
+
+    Now, how to capture the meaningful part of that, which we want to return
+    to the user? Here is a first attempt:
+    '''
+
+    user_err_msg = ""
+    error_on_previous_line = False
+    colon_on_previous_line = False
+    for line in stderr.split('\n'):
+
+        # Skip empty lines:
+        if not line:
+            continue
+
+        # Print all non-empty lines to log:
+        if log_all_lines:
+            LOGGER.error('Docker stderr: %s' % line)
+
+        # R error messages may start with the word "Error"
+        if line.startswith("Error"):
+            #LOGGER.debug('### Found explicit error line: %s' % line.strip())
+            user_err_msg += line.strip()
+            error_on_previous_line = True
+
+        # When R error messages are continued on another line, they may be
+        # indented by two spaces.
+        elif line.startswith("  ") and error_on_previous_line:
+            #LOGGER.debug('### Found indented line following an error: %s' % line.strip())
+            user_err_msg += " "+line.strip()
+            error_on_previous_line = True
+
+        # When R error messages end with a colon, they will be continued on
+        # the next line, independently of their indentation I guess!
+        elif colon_on_previous_line:
+            #LOGGER.debug('### Found line following a colon: %s' % line.strip())
+            user_err_msg += " "+line.strip()
+            error_on_previous_line = True
+
+        else:
+            #LOGGER.debug('### Do not pass back to user: %s' % line.strip())
+            error_on_previous_line = False
+
+        # Remember whether this line ended with a colon, indicating that the
+        # next line will continue with the error message:
+        colon_on_previous_line = False
+        if line.strip().endswith(":"):
+            #LOGGER.debug('### Found a colon, next line will still be error!')
+            colon_on_previous_line = True
+
+    return user_err_msg
---
+++ pygeoapi_processes/heat1.py
@@ -0,0 +1,221 @@
+import logging
+from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+LOGGER = logging.getLogger(__name__)
+
+import json
+import os
+#import sys
+import traceback
+from pygeoapi.process.HEAT.pygeoapi_processes.docker_utils import run_docker_container
+
+
+'''
+curl -X POST 'https://localhost:5000/processes/heat1/execution' \
+--header 'Content-Type: application/json' \
+--data '{
+    "inputs": {
+        "assessment_period": "holas-2"
+    }
+}'
+
+curl -X POST 'https://localhost:5000/processes/heat1/execution' \
+--header 'Content-Type: application/json' \
+--data '{
+    "inputs": {
+        "assessment_period": "2016-2021"
+    }
+}'
+
+NOTCOMMIT:
+curl -X POST 'https://aquainfra.ogc.igb-berlin.de/pygeoapi/processes/heat1/execution' \
+--header 'Content-Type: application/json' \
+--data '{
+    "inputs": {
+        "assessment_period": "holas-2"
+    }
+}'
+NOTCOMMIT:
+curl -X POST 'https://aquainfra.ogc.igb-berlin.de/pygeoapi/processes/heat1/execution' \
+--header 'Content-Type: application/json' \
+--data '{
+    "inputs": {
+        "assessment_period": "2016-2021"
+    }
+}'
+'''
+
+
+# Process metadata and description
+# Has to be in a JSON file of the same name, in the same dir! 
+script_title_and_path = __file__
+metadata_title_and_path = script_title_and_path.replace('.py', '.json')
+PROCESS_METADATA = json.load(open(metadata_title_and_path))
+
+
+
+class HEAT1Processor(BaseProcessor):
+
+    def __init__(self, processor_def):
+        super().__init__(processor_def, PROCESS_METADATA)
+        self.job_id = None
+
+        # Set config:
+        config_file_path = os.environ.get('AQUAINFRA_CONFIG_FILE', "./config.json")
+        with open(config_file_path, 'r') as config_file:
+            config = json.load(config_file)
+            self.download_dir = config["download_dir"].rstrip('/')
+            self.download_url = config["download_url"].rstrip('/')
+            self.inputs_read_only = config["helcom_heat"]["input_dir"].rstrip('/')
+            self.docker_executable = config["docker_executable"]
+            self.image_name = "heat:20250525"
+
+
+    def set_job_id(self, job_id: str):
+        self.job_id = job_id
+
+    def __repr__(self):
+        return f'<HEAT1Processor> {self.name}'
+
+
+    def execute(self, data):
+        LOGGER.info('Starting process HEAT 1!')
+        try:
+            mimetype, result = self._execute(data)
+            return mimetype, result
+
+        except Exception as e:
+            LOGGER.error(e)
+            print(traceback.format_exc())
+            raise ProcessorExecuteError(e)
+
+
+    def _execute(self, data):
+
+        ##############
+        ### Inputs ###
+        ##############
+
+        # Retrieve user inputs:
+        assessment_period = data.get('assessment_period').lower()
+
+        # Check user inputs:
+        if assessment_period is None:
+            raise ProcessorExecuteError('Missing parameter "assessment_period". Please provide a string.')
+
+        # Check validity of argument:
+        valid_assessment_periods = ["holas-2", "holas-3", "other"]
+        if not assessment_period in valid_assessment_periods:
+            raise ValueError('assessment_period is "%s", must be one of: %s' % (assessment_period, valid_assessment_periods))
+
+        # Assign years to selected assessment period:
+        if assessment_period == 'holas-2':
+            assessment_period = '2011-2016'
+        elif assessment_period == 'holas-3':
+            assessment_period = '2016-2021'
+        elif assessment_period == 'other':
+            assessment_period = '1877-9999'
+
+
+        ##################
+        ### Input data ###
+        ##################
+
+        # Directory where static input data can be found. It will be mounted read-only to the container:
+        path_input_data = self.inputs_read_only
+
+        # Define paths to static input paths depending on assessment_period
+        in_unitsFilePath = get_unit_file_path(assessment_period, path_input_data)
+        in_unitGridSizePath = get_gridsize_file_path(assessment_period, path_input_data)
+
+
+        ###############
+        ### Outputs ###
+        ###############
+
+        # Where to store output data
+        out_units_gridded_filepath = self.download_dir+'/out/units_gridded-%s.shp' % self.job_id
+        out_units_cleaned_filepath = self.download_dir+'/out/units_cleaned-%s.shp' % self.job_id
+        # Where to access output data
+        out_units_gridded_url      = self.download_url+'/out/'+out_units_gridded_filepath.split('/')[-1]
+        out_units_cleaned_url      = self.download_url+'/out/'+out_units_cleaned_filepath.split('/')[-1]
+
+        ###########
+        ### Run ###
+        ###########
+
+        # Actually call R script:
+        script_name = 'run_heat1_csv.R'
+        r_args = [assessment_period, in_unitsFilePath, in_unitGridSizePath, out_units_cleaned_filepath, out_units_gridded_filepath]
+        returncode, stdout, stderr, user_err_msg = run_docker_container(
+            self.docker_executable,
+            self.image_name,
+            script_name,
+            self.job_id,
+            self.download_dir,
+            self.inputs_read_only,
+            r_args
+        )
+        # The results are two shapefiles:
+        # * units_cleaned.shp
+        # * units_gridded.shp
+        # and five maps of the Assessment Units:
+        # * Assessment_Units.png
+        # * Assessment_GridUnits10.png
+        # * Assessment_GridUnits30.png
+        # * Assessment_GridUnits60.png
+        # * Assessment_GridUnits.png
+        # TODO Discuss: Return maps as GeoJSON? Those are static, could just be downloaded. Unless we allow other assessment units one day.
+
+
+        # Return R error message if exit code not 0:
+        if not returncode == 0:
+            raise ProcessorExecuteError(user_msg = user_err_msg)
+
+
+        ######################
+        ### Return results ###
+        ######################
+
+        # Return output csv file as string directly in HTTP payload:
+        # TODO check requested_outputs for user preference!
+        
+        # Or return link to output csv file and return it wrapped in JSON:
+        # TODO: add png, maybe cleaned
+        # TODO: Returning shp makes no sense without all the other files!!! 
+        outputs = {
+            "outputs": {
+                "units_gridded": {
+                    "title": PROCESS_METADATA['outputs']['units_gridded']['title'],
+                    "description": PROCESS_METADATA['outputs']['units_gridded']['description'],
+                    "href": out_units_gridded_url
+                },
+                "units_cleaned": {
+                    "title": PROCESS_METADATA['outputs']['units_cleaned']['title'],
+                    "description": PROCESS_METADATA['outputs']['units_cleaned']['description'],
+                    "href": out_units_cleaned_url
+                }
+            }
+        }
+
+        return 'application/json', outputs
+
+
+def get_unit_file_path(assessment_period, path_input_data):
+
+    if assessment_period == "1877-9999":
+        return path_input_data+"/1877-9999/HELCOM_subbasin_with_coastal_WFD_waterbodies_or_watertypes_2022_eutro.shp"
+    elif assessment_period == "2011-2016":
+        return path_input_data+"/2011-2016/AssessmentUnits.shp"
+    elif assessment_period == "2016-2021":
+        return path_input_data+"/2016-2021/HELCOM_subbasin_with_coastal_WFD_waterbodies_or_watertypes_2022_eutro.shp"
+
+
+def get_gridsize_file_path(assessment_period, path_input_data):
+
+    if assessment_period == "1877-9999":
+        return path_input_data+"/adapted_inputs/1877-9999/Configuration1877-9999_UnitGridSize.csv"
+    elif assessment_period == "2011-2016":
+        return path_input_data+"/adapted_inputs/2011-2016/Configuration2011-2016_UnitGridSize.csv"
+    elif assessment_period == "2016-2021":
+        return path_input_data+"/adapted_inputs/2016-2021/Configuration2016-2021_UnitGridSize.csv"
+
