097e48fb19943ecc05c222142e5163e7d191f31b
Merret <merret.buurman@igb-berlin.de>
2025-10-10 20:40:28 +0200

    Downloading zipped shape: Not relying anymore on zip file ending.

--- src/utils_download.R
+++ src/utils_download.R
@@ -17,21 +17,36 @@ download_unzip_shapefile_tmp <- function(url) {
 
 download_unzip_shapefile <- function(url, targetpath) {
   # If the URL points to a zipped shapefile, download and unzip before we can read it:
-  if (startsWith(url, 'http') & endsWith(url, 'zip')) {
+  # Note: URLs don't have to end on .zip to contain zipped shapefiles, so just try and error:
+  #if (startsWith(url, 'http') & endsWith(url, 'zip')) {
+  if (startsWith(url, 'http')) {
     message("DEBUG: Downloading zipped shapefile from: ", url)
     message("DEBUG: Downloading zipped shapefile to  : ", targetpath)
     #temp_zip <- tempfile(fileext = ".zip")
     #download.file(url, temp_zip, mode = "wb")
     #unzip(temp_zip, exdir = tempdir())
     download.file(url, targetpath, mode = "wb")
-    unzip(targetpath, exdir=dirname(targetpath))
+    message("DEBUG: Downloaded... ", url)
+    #unzip(targetpath, exdir=dirname(targetpath))
+    tryCatch(
+      {
+        newpath <- unzip(targetpath, exdir=dirname(targetpath))
+        message("DEBUG: This was a zip, unzipped to: ", newpath)
+        #targetpath <- newpath
+      }, error = function(e){
+        message("ERROR: This was not a zip: ", targetpath)
+        stop('If you specify a remote shapefile as input, please zip it...')
+      }, warning = function(e){
+        message("WARN: This was not a zip: ", targetpath)
+        stop('If you specify a remote shapefile as input, please zip it...')
+      }
+    )
+    message("DEBUG: Unzipped... ", targetpath)
+    # Note: The following only works if we only download one shapefile to targetpath, I guess...
     shapefile_path <- list.files(dirname(targetpath), pattern = "\\.shp$", full.names = TRUE)
     message("DEBUG: Downloading and unzipping zipped shapefile... DONE.")
-  # If the URL points to a non-zipped shapefile, throw error:
-  } else if (startsWith(url, 'http') & endsWith(url, 'shp')) {
-    stop('If you specify a remote shapefile as input, please zip it...')
-  }
   return(shapefile_path)
+  }
 }
 
 download_maybe_unzip <- function(url, targetpath) {
